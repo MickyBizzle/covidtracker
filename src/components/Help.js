a852febab5c4f9dd90ce7ac6977beb0ecb6ebe79
mab3103 <mikeistheman@hotmail.co.uk>
2020-03-28 22:58:14 +0000

    Help comes up on page load

--- src/components/Help.js
+++ src/components/Help.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Help.scss";
 
 export const Help = () => {
-  const [hovering, setHovering] = useState(false);
+  const [hovering, setHovering] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
