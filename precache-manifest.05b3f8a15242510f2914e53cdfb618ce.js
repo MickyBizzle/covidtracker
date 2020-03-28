c939e08ba107437e500fc6bcb3520c666e45558d
mab3103 <mikeistheman@hotmail.co.uk>
2020-03-28 22:39:48 +0000

    Updates

---
+++ precache-manifest.05b3f8a15242510f2914e53cdfb618ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a011c86a209d86e2f0a14ddc1e4ea36c",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "9e24eb2fad4011fa7383",
+    "url": "/covidtracker/static/css/main.f36f85b3.chunk.css"
+  },
+  {
+    "revision": "b53221e8f0202eff8aeb",
+    "url": "/covidtracker/static/js/2.26bd170f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidtracker/static/js/2.26bd170f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e24eb2fad4011fa7383",
+    "url": "/covidtracker/static/js/main.30e74da9.chunk.js"
+  },
+  {
+    "revision": "539338f4cc1816095e99",
+    "url": "/covidtracker/static/js/runtime-main.edd942bb.js"
+  }
+]);
